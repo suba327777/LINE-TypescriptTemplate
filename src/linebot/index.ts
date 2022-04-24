504d7ff133d8937921c147dc9b14779bf7f4f743
suba327777 <suba.1357@ezweb.ne.jp>
2022-04-24 14:52:04 +0900

    clean server #5

--- src/linebot/index.ts
+++ src/linebot/index.ts
@@ -6,15 +6,18 @@ import { lineMiddlewareConfig } from "../lib/line";
 /* handlers */
 import { handlers } from "./handlers";
 
-const PORT = process.env.PORT || 3000;
+export const app: express.Express = express();
 
-const app: express.Express = express();
-
+// Test Routing
+app.get("/", (req: express.Request, res: express.Response): void => {
+  res.send("hello");
+});
 // App Routing
 app.use(middleware(lineMiddlewareConfig));
-app.post("/", async (req: express.Request, res: express.Response): Promise<void> => {
+app.post("/webhook", async (req: express.Request, res: express.Response): Promise<void> => {
   try {
     res.status(200).end();
+    console.log("aa");
     const events: WebhookEvent[] = req.body.events;
     events.map(async (event: WebhookEvent): Promise<void> => {
       handlers(event);
@@ -23,9 +26,4 @@ app.post("/", async (req: express.Request, res: express.Response): Promise<void>
     console.log(err);
     res.status(500).end();
   }
-});
-
-// Start the server
-app.listen(PORT, (): void => {
-  console.log(`Application is live and listening on port ${PORT}`);
 });
