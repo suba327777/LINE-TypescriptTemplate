c73644dc79118b20749885927ead18043422894d
suba327777 <suba.1357@ezweb.ne.jp>
2022-05-05 17:32:27 +0900

    clean file

--- src/linebot/handlers/follow.ts
+++ src/linebot/handlers/follow.ts
@@ -3,7 +3,7 @@ import { FollowEvent } from "@line/bot-sdk";
 /* lib */
 import { lineClient } from "../../lib/line";
 /* message */
-import { followMessage } from "../messages/followMessage";
+import { followMessage } from "../notice-messages/follow";
 
 export const followHandler = async (event: FollowEvent): Promise<void> => {
   await lineClient.replyMessage(event.replyToken, followMessage);
--- src/linebot/handlers/index.ts
+++ src/linebot/handlers/index.ts
@@ -3,15 +3,18 @@ import { WebhookEvent } from "@line/bot-sdk";
 /* lib */
 import { lineClient } from "../../lib/line";
 /* handler */
-import { followHandler } from "./followHandler";
-/* messages */
-import { errorMessage } from "../messages/errorMessage";
+import { followHandler } from "./follow";
+import { messagesHandler } from "./messages";
+/* notice-messages */
+import { errorMessage } from "../notice-messages/error";
 
 export const handlers = async (event: WebhookEvent): Promise<void> => {
   try {
     switch (event.type) {
       case "follow":
         return await followHandler(event);
+      case "message":
+        return await messagesHandler(event);
     }
   } catch (err: unknown) {
     lineClient.pushMessage(event.source.userId!, errorMessage).catch;
